ee7a62354f675b3ccda256eb0ef9d725e4383e6b
Srimans7 <139859640+Srimans7@users.noreply.github.com>
2023-09-28 21:09:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,21 +7,24 @@ import Employee from './employee.js';
 import Nav from './nav.js'; 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+
 const myStyle = {
-    backgroundImage:
-        "url('https://wallpapers.com/images/hd/light-blue-white-gradient-ocp1nxf9y5f7ae47.jpg')",
+    backgroundColor:
+        "#D3D3D3",
     
-}; 
-
-
+};
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<div style={myStyle}><BrowserRouter>
-    <Nav />
+
+	<div style={{display: 'flex' }} >
+    <Nav  />
+
     <Routes>
         <Route exact path='/' exact element={<Team />} />
         <Route path='/projects' element={<Project />} />
         <Route  path='/employees'  element={<Employee />} />
     </Routes>
+    </div>
 </BrowserRouter></div>
   
 );
